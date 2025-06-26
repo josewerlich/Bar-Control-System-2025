e3c5161aa5a33efdd02a54b7c1423429ad4e18b5
josewerlich <werlich@outlook.com>
2025-06-26 17:28:16 -0400

    Creation of the products class

--- ProductsModule/Product.cs
+++ ProductsModule/Product.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace Bar_Control_System_2025.ProductsModule
 {
-    class Product
+    public class Product : BaseEntity<Product>
     {
+        public string Name;
+        public decimal Cost;
+
+        public Product(string name, decimal cost)
+        {
+            Name = name;
+            Cost = cost;
+        }
+
+        public override void UpdateRegister(Product updateRegister)
+        {
+            Name = updateRegister.Name;
+            Cost = updateRegister.Cost;
+        }
+
+        public override string Validate()
+        {
+            string errors = string.Empty;
+
+            if (Name.Length < 2 || Name.Length > 100)
+                errors += "The field \"Name\"  must have between 3 and 100 characters.";
+
+            if (Cost == 0.0m)
+                errors += "The field \"Cost\" must have a positive number.";
+
+            return errors;
+        }
     }
 }
