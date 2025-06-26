a76f9e204bba9c30b0c5c4777f10ac337b485952
josewerlich <werlich@outlook.com>
2025-06-26 17:47:15 -0400

    Created class AccountRepository

--- AccountModule/AccountRepository.cs
+++ AccountModule/AccountRepository.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using static System.Net.Mime.MediaTypeNames;
 
-namespace Bar_Control_System_2025.AccountModule
-{
-    class AccountRepository
+namespace Bar_Control_System_2025.AccountModule;
+
+    public class AccountRepository
     {
+        protected Account[] registers = new Account[100];
+        protected int registerCounter = 0;
+        protected int idCounter = 0;
+
+        public void Register(Account newAccount)
+        {
+            newAccount.id = ++idCounter;
+
+            registers[registerCounter++] = newAccount;
+        }
+
+        public Account[] SelectAccount()
+        {
+            return registers;
+        }
+
+        public Account SelectAccountViaID(int idSelected)
+        {
+            for (int i = 0; i < registers.Length; i++)
+            {
+                Account register = registers[i];
+
+                if (register == null)
+                    continue;
+
+                if (register.id == idSelected)
+                    return register;
+            }
+
+            return null;
+        }
     }
-}
